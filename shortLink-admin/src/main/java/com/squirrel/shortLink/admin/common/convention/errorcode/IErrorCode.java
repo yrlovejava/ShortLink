3ee8abf792a0d774a3a94e3b46222af16c81a7b7
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-14 22:54:23 +0800

    定义用户自定义异常码和全局异常拦截器

---
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,19 @@
+package com.squirrel.shortLink.admin.common.convention.errorcode;
+
+/**
+ * 平台错误码
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     * @return code
+     */
+    String code();
+
+    /**
+     * 错误信息
+     * @return message
+     */
+    String message();
+}
---
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/common/convention/result/Results.java
@@ -0,0 +1,70 @@
+package com.squirrel.shortLink.admin.common.convention.result;
+
+import com.squirrel.shortLink.admin.common.convention.errorcode.BaseErrorCode;
+import com.squirrel.shortLink.admin.common.convention.exception.AbstractException;
+
+import java.util.Optional;
+
+/**
+ * 返回全局对象构造器
+ */
+public final class Results {
+
+    /**
+     * 构造成功响应
+     * @return 成功的Result
+     */
+    public static Result<Void> success() {
+        return new Result<Void>()
+                .setCode(Result.SUCCESS_CODE);
+    }
+
+    /**
+     * 构造带返回数据的成功响应
+     * @param data 数据
+     * @return Result
+     * @param <T> 泛型
+     */
+    public static <T> Result<T> success(T data) {
+        return new Result<T>()
+                .setCode(Result.SUCCESS_CODE)
+                .setData(data);
+    }
+
+    /**
+     * 构造服务端失败影响
+     * @return Result
+     */
+    public static Result<Void> failure() {
+        return new Result<Void>()
+                .setCode(BaseErrorCode.SERVICE_ERROR.code())
+                .setMessage(BaseErrorCode.SERVICE_ERROR.message());
+    }
+
+    /**
+     * 通过 {@link AbstractException} 构建失败响应
+     * @param abstractException AbstractException
+     * @return 失败响应
+     */
+    public static Result<Void> failure(AbstractException abstractException) {
+        String errorCode = Optional.ofNullable(abstractException.getErrorCode())
+                .orElse(BaseErrorCode.SERVICE_ERROR.code());
+        String errorMessage = Optional.ofNullable(abstractException.getErrorMessage())
+                .orElse(BaseErrorCode.SERVICE_ERROR.message());
+        return new Result<Void>()
+                .setCode(errorCode)
+                .setMessage(errorMessage);
+    }
+
+    /**
+     * 通过 errorCode、errorMessage 构建失败响应
+     * @param errorCode 失败码
+     * @param errorMessage 错误信息
+     * @return 失败响应
+     */
+    public static Result<Void> failure(String errorCode,String errorMessage) {
+        return new Result<Void>()
+                .setCode(errorCode)
+                .setMessage(errorMessage);
+    }
+}
