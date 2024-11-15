facb4b800a4c292d5603c88c91a8439da7777a82
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-15 17:36:44 +0800

    通过分布式锁完善用户注册功能

---
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,12 @@
+package com.squirrel.shortLink.admin.common.constant;
+
+/**
+ * 短链接后管 Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁的key
+     */
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register";
+}
--- shortLink-admin/src/main/java/com/squirrel/shortLink/admin/service/impl/UserServiceImpl.java
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/service/impl/UserServiceImpl.java
@@ -11,10 +11,13 @@ import com.squirrel.shortLink.admin.dto.resp.UserRespDTO;
 import com.squirrel.shortLink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import static com.squirrel.shortLink.admin.common.enums.UserErrorCodeEnum.*;
+import static com.squirrel.shortLink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 
 /**
  * 用户接口实现层
@@ -24,6 +27,7 @@ import static com.squirrel.shortLink.admin.common.enums.UserErrorCodeEnum.*;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     /**
      * 根据用户名查询用户信息
@@ -64,12 +68,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUsername(requestParam.getUsername())){
             throw new ClientException(USER_NAME_EXIST);
         }
-        // 2.向数据库中插入数据
-        int inserted = getBaseMapper().insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if (inserted < 1){
-            throw new ClientException(USER_SAVE_ERROR);
+        // 2.加分布式锁，向数据库中插入数据
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                int inserted = getBaseMapper().insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (inserted < 1){
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                // 3.在布隆过滤器中保存新的用户名
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        }finally {
+            lock.unlock();
         }
-        // 3.在布隆过滤器中添加新用户名信息
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
