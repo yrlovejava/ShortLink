e80fb7f58156453f9ce566c2713a0f3b12bcbd99
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-16 12:11:04 +0800

    添加依赖，删除不必要的@Mapper注解

--- shortLink-admin/src/main/java/com/squirrel/shortLink/admin/dao/mapper/GroupMapper.java
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/dao/mapper/GroupMapper.java
@@ -2,12 +2,10 @@ package com.squirrel.shortLink.admin.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.squirrel.shortLink.admin.dao.entity.GroupDO;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 短链接分组持久层
  */
-@Mapper
 public interface GroupMapper extends BaseMapper<GroupDO> {
 
 }
--- shortLink-admin/src/main/java/com/squirrel/shortLink/admin/dao/mapper/UserMapper.java
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/dao/mapper/UserMapper.java
@@ -2,11 +2,9 @@ package com.squirrel.shortLink.admin.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.squirrel.shortLink.admin.dao.entity.UserDO;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 用户持久层
  */
-@Mapper
 public interface UserMapper extends BaseMapper<UserDO> {
 }
