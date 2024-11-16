4fdc674373675679bc06a54e7473bb7908f26a15
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-16 12:07:31 +0800

    开发用户登录验证拦截器返回友好提示信息

--- shortLink-admin/src/main/java/com/squirrel/shortLink/admin/common/biz/user/UserTransmitFilter.java
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,13 +1,23 @@
 package com.squirrel.shortLink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
+import com.squirrel.shortLink.admin.common.convention.exception.ClientException;
+import com.squirrel.shortLink.admin.common.convention.result.Results;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 import java.util.Objects;
+
+import static com.squirrel.shortLink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -17,6 +27,11 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/admin/v1/actual/user/has-username"
+    );
+
     /**
      * 过滤器做登录校验和在上下文中封装用户信息
      * @param servletRequest 这里肯定是http请求
@@ -25,17 +40,36 @@ public class UserTransmitFilter implements Filter {
      * @throws IOException 异常
      * @throws ServletException 异常
      */
+    @SneakyThrows
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         // 1.获取请求路劲
         String requestURI = httpServletRequest.getRequestURI();
-        // 2.判断是否是登录请求，如果不是登录请求就封装用户信息上下文
-        if(!Objects.equals(requestURI, "/api/short-link/v1/user/login")){
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+        // 2.判断是否是需要忽略的路径
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if(!Objects.equals(requestURI, "/api/short-link/v1/user") && Objects.equals(method,"POST")){
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(
+                            Results.failure(new ClientException(USER_TOKEN_FAIL))
+                    ));
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        throw new ClientException(USER_TOKEN_FAIL);
+                    }
+                }catch (Exception ex){
+                    returnJson((HttpServletResponse) servletResponse,  JSON.toJSONString(
+                            Results.failure(new ClientException(USER_TOKEN_FAIL))
+                    ));
+                    return;
+                }
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
@@ -44,6 +78,20 @@ public class UserTransmitFilter implements Filter {
             filterChain.doFilter(servletRequest, servletResponse);
         }finally {
             UserContext.removeUser();
+        }
+    }
+
+    /**
+     * 通过浏览器返回 json 字符串
+     * @param response http响应
+     * @param json json字符串
+     */
+    private void returnJson(HttpServletResponse response,String json) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        } catch (IOException ignored) {
         }
     }
 }
