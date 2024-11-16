d250899ec40a8ae885e7aa00f426e7a967361f83
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-16 16:58:40 +0800

    新增mybatis-plus分页插件

---
+++ shortLink-project/src/main/java/com/squirrel/project/config/DataBaseConfiguration.java
@@ -0,0 +1,21 @@
+package com.squirrel.project.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     * @return MybatisPlusInterceptor
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
