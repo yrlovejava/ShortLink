9b7505db9c742dd43d590a784a3c7f610097ea37
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-19 09:21:14 +0800

    开发短链接新增回收功能

--- shortLink-project/src/main/java/com/squirrel/project/controller/RecycleBinController.java
+++ shortLink-project/src/main/java/com/squirrel/project/controller/RecycleBinController.java
@@ -1,7 +1,9 @@
 package com.squirrel.project.controller;
 
 import com.squirrel.common.convention.result.Result;
+import com.squirrel.common.convention.result.Results;
 import com.squirrel.project.dto.req.RecycleBinSaveReqDTO;
+import com.squirrel.project.service.RecycleBinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RecycleBinController {
 
+    private final RecycleBinService recycleBinService;
+
     /**
      * 保存回收站
      * @param requestParam 回收站保存参数
@@ -21,6 +25,7 @@ public class RecycleBinController {
      */
     @PostMapping("/api/short-link/v1/recycle-bin/save")
     public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
-
+        recycleBinService.saveRecycleBin(requestParam);
+        return Results.success();
     }
 }
