6b5a361b52af2f7924a82faa90af53715c59d9e4
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-22 14:32:54 +0800

    开发短链接监控之今日统计

--- shortLink-project/src/main/java/com/squirrel/project/dao/entity/LinkStatsTodayDO.java
+++ shortLink-project/src/main/java/com/squirrel/project/dao/entity/LinkStatsTodayDO.java
@@ -5,7 +5,10 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.squirrel.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -14,6 +17,9 @@ import java.util.Date;
  */
 @Data
 @TableName("t_link_stats_today")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -56,5 +62,5 @@ public class LinkStatsTodayDO extends BaseDO {
      * 今日ip数
      */
     @TableField("today_uip")
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
--- shortLink-project/src/main/java/com/squirrel/project/dao/mapper/LinkStatsTodayMapper.java
+++ shortLink-project/src/main/java/com/squirrel/project/dao/mapper/LinkStatsTodayMapper.java
@@ -2,9 +2,16 @@ package com.squirrel.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.squirrel.project.dao.entity.LinkStatsTodayDO;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 短链接今天统计持久层
  */
 public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+
+    /**
+     * 记录今日统计监控数据
+     * @param linkStatsTodayDO 今日监控数据
+     */
+    void shortLinkTodayState(@Param("linkTodayStats") LinkStatsTodayDO linkStatsTodayDO);
 }
