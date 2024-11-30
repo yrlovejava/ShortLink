f60bba65dda0086f6a98e228a7566e1b776c6868
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-11-30 18:52:52 +0800

    给消息队列消费任务在数据库中记录短链接监控数据添加事务

--- shortLink-project/src/main/java/com/squirrel/shortLink/project/mq/consumer/ShortLinkStatsSaveConsumer.java
+++ shortLink-project/src/main/java/com/squirrel/shortLink/project/mq/consumer/ShortLinkStatsSaveConsumer.java
@@ -18,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.stream.MapRecord;
 import org.springframework.data.redis.connection.stream.RecordId;
@@ -99,135 +100,146 @@ public class ShortLinkStatsSaveConsumer implements StreamListener<String, MapRec
         RReadWriteLock readWriteLock = redissonClient.getReadWriteLock(String.format(LOCK_GID_UPDATE_KEY, fullShortUrl));
         RLock rLock = readWriteLock.readLock();
         rLock.lock();
-        // TODO 需要加事务控制,而且事务一定要在锁释放之前
         try {
-            // 2.从路由表中通过完整短链接查询goto实体
-            ShortLinkGotoDO shortLinkGotoDO = shortLinkGotoMapper.selectOne(Wrappers.<ShortLinkGotoDO>lambdaQuery()
-                    .eq(ShortLinkGotoDO::getFullShortUrl, fullShortUrl));
-            // 通过goto实体获取到分组id
-            String gid = shortLinkGotoDO.getGid();
-
-            // 3.获取当前时间信息
-            // 当前日期
-            Date currentDate = statsRecord.getCurrentDate();
-            // 小时(24小时制)
-            int hour = DateUtil.hour(new Date(),true);
-            // 星期
-            Week week = DateUtil.dayOfWeekEnum(new Date());
-            int weekValue = week.getIso8601Value();// 国际标准日期
-
-            // 4.构造短链接访问实体，插入数据库
-            LinkAccessStatsDO linkAccessStatsDO = LinkAccessStatsDO.builder()
-                    .pv(1)
-                    .uv(statsRecord.getUvFirstFlag() ? 1 : 0)
-                    .uip(statsRecord.getUipFirstFlag() ? 1 : 0)
-                    .hour(hour)
-                    .weekday(weekValue)
-                    .fullShortUrl(fullShortUrl)
-                    .gid(gid)
-                    .date(currentDate)
-                    .build();
-            linkAccessStatsMapper.insert(linkAccessStatsDO);
-
-            // 5.调用高德开放api接口，解析ip的所处地址
-            Map<String,Object> localeParamMap = new HashMap<>();
-            localeParamMap.put("key",statsLocaleAmapKey);
-            localeParamMap.put("ip",statsRecord.getRemoteAddr());
-            String localResultStr = HttpUtil.get(AMAP_REMOTE_URL, localeParamMap);
-            JSONObject localeResultObj = JSON.parseObject(localResultStr);
-            // infocode 调用结果状态码
-            String infocode = localeResultObj.getString("infocode");
-            String actualProvince = "未知";
-            String actualCity = "未知";
-            // 如果状态码为 10000 证明调用成功
-            if (StrUtil.isNotBlank(infocode) && StrUtil.equals(infocode,"10000")) {
-                // 获取省份
-                String province = localeResultObj.getString("province");
-                // 获取城市
-                String city = localeResultObj.getString("city");
-                // 判断是否为空
-                boolean unknownFlag = StrUtil.equals(province,"[]");
-                // 6.构造地区统计实体并插入数据库
-                LinkLocaleStatsDO linkLocaleStatsDO = LinkLocaleStatsDO.builder()
-                        .province(actualProvince = unknownFlag ? actualProvince : province)
-                        .city(actualCity = unknownFlag ? actualCity : city)
-                        .cnt(1)
-                        .fullShortUrl(fullShortUrl)
-                        .country("中国")
-                        .gid(gid)
-                        .date(currentDate)
-                        .build();
-                linkLocaleStatsMapper.insert(linkLocaleStatsDO);
-            }
-
-            // 7.构造操作系统统计实体，并插入数据库
-            LinkOsStatsDO linkOsStatsDO = LinkOsStatsDO.builder()
-                    .os(statsRecord.getOs())
-                    .cnt(1)
-                    .gid(gid)
-                    .fullShortUrl(fullShortUrl)
-                    .date(currentDate)
-                    .build();
-            linkOsStatsMapper.insert(linkOsStatsDO);
-
-            // 8.构造浏览器统计实体，并插入数据库
-            LinkBrowserStatsDO linkBrowserStatsDO = LinkBrowserStatsDO.builder()
-                    .browser(statsRecord.getBrowser())
-                    .cnt(1)
-                    .gid(gid)
-                    .fullShortUrl(fullShortUrl)
-                    .date(currentDate)
-                    .build();
-            linkBrowserStatsMapper.shortLinkBrowserStats(linkBrowserStatsDO);
-
-            // 9.构造设备统计实体，并插入数据库
-            LinkDeviceStatsDO linkDeviceStatsDO = LinkDeviceStatsDO.builder()
-                    .device(statsRecord.getDevice())
-                    .cnt(1)
-                    .gid(gid)
-                    .fullShortUrl(fullShortUrl)
-                    .date(currentDate)
-                    .build();
-            linkDeviceStatsMapper.shortLinkDeviceState(linkDeviceStatsDO);
-
-            // 10.构造网络统计实体，并插入数据库
-            LinkNetworkStatsDO linkNetworkStatsDO = LinkNetworkStatsDO.builder()
-                    .network(statsRecord.getNetwork())
-                    .cnt(1)
-                    .gid(gid)
-                    .fullShortUrl(fullShortUrl)
-                    .date(currentDate)
-                    .build();
-            linkNetworkStatsMapper.shortLinkNetworkState(linkNetworkStatsDO);
-
-            // 11.构造访问日志统计实体，并插入数据库
-            LinkAccessLogsDO linkAccessLogsDO = LinkAccessLogsDO.builder()
-                    .user(statsRecord.getUv())
-                    .ip(statsRecord.getRemoteAddr())
-                    .browser(statsRecord.getBrowser())
-                    .os(statsRecord.getOs())
-                    .network(statsRecord.getNetwork())
-                    .device(statsRecord.getDevice())
-                    .locale(StrUtil.join("-", "中国", actualProvince, actualCity))
-                    .fullShortUrl(fullShortUrl)
-                    .build();
-            linkAccessLogsMapper.insert(linkAccessLogsDO);
-
-            // 12.在短链接表中自增访问次数
-            shortLinkMapper.incrementStats(gid,fullShortUrl,1,statsRecord.getUvFirstFlag() ? 1 : 0,statsRecord.getUipFirstFlag() ? 1 : 0);
-
-            // 13.构造今日访问实体，并插入数据库
-            LinkStatsTodayDO linkStatsTodayDO = LinkStatsTodayDO.builder()
-                    .todayPv(1)
-                    .todayUv(statsRecord.getUvFirstFlag() ? 1 : 0)
-                    .todayUip(statsRecord.getUipFirstFlag() ? 1 : 0)
-                    .gid(gid)
-                    .fullShortUrl(fullShortUrl)
-                    .date(new Date())
-                    .build();
-            linkStatsTodayMapper.shortLinkTodayState(linkStatsTodayDO);
+            // 使用AOPContext 获取代理对象来调用saveShortLinkStats 方法
+            ShortLinkStatsSaveConsumer proxy = (ShortLinkStatsSaveConsumer) AopContext.currentProxy();
+            proxy.saveShortLinkStats(statsRecord,fullShortUrl);
         } finally {
             rLock.unlock();
         }
+    }
+
+    /**
+     * 在数据库中保存短链接监控数据
+     * @param statsRecord 短链接记录参数
+     * @param fullShortUrl 完整短链接
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public void saveShortLinkStats(ShortLinkStatsRecordDTO statsRecord, String fullShortUrl) {
+        // 2.从路由表中通过完整短链接查询goto实体
+        ShortLinkGotoDO shortLinkGotoDO = shortLinkGotoMapper.selectOne(Wrappers.<ShortLinkGotoDO>lambdaQuery()
+                .eq(ShortLinkGotoDO::getFullShortUrl, fullShortUrl));
+        // 通过goto实体获取到分组id
+        String gid = shortLinkGotoDO.getGid();
+
+        // 3.获取当前时间信息
+        // 当前日期
+        Date currentDate = statsRecord.getCurrentDate();
+        // 小时(24小时制)
+        int hour = DateUtil.hour(new Date(),true);
+        // 星期
+        Week week = DateUtil.dayOfWeekEnum(new Date());
+        int weekValue = week.getIso8601Value();// 国际标准日期
+
+        // 4.构造短链接访问实体，插入数据库
+        LinkAccessStatsDO linkAccessStatsDO = LinkAccessStatsDO.builder()
+                .pv(1)
+                .uv(statsRecord.getUvFirstFlag() ? 1 : 0)
+                .uip(statsRecord.getUipFirstFlag() ? 1 : 0)
+                .hour(hour)
+                .weekday(weekValue)
+                .fullShortUrl(fullShortUrl)
+                .gid(gid)
+                .date(currentDate)
+                .build();
+        linkAccessStatsMapper.insert(linkAccessStatsDO);
+
+        // 5.调用高德开放api接口，解析ip的所处地址
+        Map<String,Object> localeParamMap = new HashMap<>();
+        localeParamMap.put("key",statsLocaleAmapKey);
+        localeParamMap.put("ip", statsRecord.getRemoteAddr());
+        String localResultStr = HttpUtil.get(AMAP_REMOTE_URL, localeParamMap);
+        JSONObject localeResultObj = JSON.parseObject(localResultStr);
+        // infocode 调用结果状态码
+        String infocode = localeResultObj.getString("infocode");
+        String actualProvince = "未知";
+        String actualCity = "未知";
+        // 如果状态码为 10000 证明调用成功
+        if (StrUtil.isNotBlank(infocode) && StrUtil.equals(infocode,"10000")) {
+            // 获取省份
+            String province = localeResultObj.getString("province");
+            // 获取城市
+            String city = localeResultObj.getString("city");
+            // 判断是否为空
+            boolean unknownFlag = StrUtil.equals(province,"[]");
+            // 6.构造地区统计实体并插入数据库
+            LinkLocaleStatsDO linkLocaleStatsDO = LinkLocaleStatsDO.builder()
+                    .province(actualProvince = unknownFlag ? actualProvince : province)
+                    .city(actualCity = unknownFlag ? actualCity : city)
+                    .cnt(1)
+                    .fullShortUrl(fullShortUrl)
+                    .country("中国")
+                    .gid(gid)
+                    .date(currentDate)
+                    .build();
+            linkLocaleStatsMapper.insert(linkLocaleStatsDO);
+        }
+
+        // 7.构造操作系统统计实体，并插入数据库
+        LinkOsStatsDO linkOsStatsDO = LinkOsStatsDO.builder()
+                .os(statsRecord.getOs())
+                .cnt(1)
+                .gid(gid)
+                .fullShortUrl(fullShortUrl)
+                .date(currentDate)
+                .build();
+        linkOsStatsMapper.insert(linkOsStatsDO);
+
+        // 8.构造浏览器统计实体，并插入数据库
+        LinkBrowserStatsDO linkBrowserStatsDO = LinkBrowserStatsDO.builder()
+                .browser(statsRecord.getBrowser())
+                .cnt(1)
+                .gid(gid)
+                .fullShortUrl(fullShortUrl)
+                .date(currentDate)
+                .build();
+        linkBrowserStatsMapper.shortLinkBrowserStats(linkBrowserStatsDO);
+
+        // 9.构造设备统计实体，并插入数据库
+        LinkDeviceStatsDO linkDeviceStatsDO = LinkDeviceStatsDO.builder()
+                .device(statsRecord.getDevice())
+                .cnt(1)
+                .gid(gid)
+                .fullShortUrl(fullShortUrl)
+                .date(currentDate)
+                .build();
+        linkDeviceStatsMapper.shortLinkDeviceState(linkDeviceStatsDO);
+
+        // 10.构造网络统计实体，并插入数据库
+        LinkNetworkStatsDO linkNetworkStatsDO = LinkNetworkStatsDO.builder()
+                .network(statsRecord.getNetwork())
+                .cnt(1)
+                .gid(gid)
+                .fullShortUrl(fullShortUrl)
+                .date(currentDate)
+                .build();
+        linkNetworkStatsMapper.shortLinkNetworkState(linkNetworkStatsDO);
+
+        // 11.构造访问日志统计实体，并插入数据库
+        LinkAccessLogsDO linkAccessLogsDO = LinkAccessLogsDO.builder()
+                .user(statsRecord.getUv())
+                .ip(statsRecord.getRemoteAddr())
+                .browser(statsRecord.getBrowser())
+                .os(statsRecord.getOs())
+                .network(statsRecord.getNetwork())
+                .device(statsRecord.getDevice())
+                .locale(StrUtil.join("-", "中国", actualProvince, actualCity))
+                .fullShortUrl(fullShortUrl)
+                .build();
+        linkAccessLogsMapper.insert(linkAccessLogsDO);
+
+        // 12.在短链接表中自增访问次数
+        shortLinkMapper.incrementStats(gid, fullShortUrl,1, statsRecord.getUvFirstFlag() ? 1 : 0, statsRecord.getUipFirstFlag() ? 1 : 0);
+
+        // 13.构造今日访问实体，并插入数据库
+        LinkStatsTodayDO linkStatsTodayDO = LinkStatsTodayDO.builder()
+                .todayPv(1)
+                .todayUv(statsRecord.getUvFirstFlag() ? 1 : 0)
+                .todayUip(statsRecord.getUipFirstFlag() ? 1 : 0)
+                .gid(gid)
+                .fullShortUrl(fullShortUrl)
+                .date(new Date())
+                .build();
+        linkStatsTodayMapper.shortLinkTodayState(linkStatsTodayDO);
     }
 }
