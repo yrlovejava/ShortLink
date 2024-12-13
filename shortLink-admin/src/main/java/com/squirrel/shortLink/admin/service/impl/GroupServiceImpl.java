c69771f23ea6940f70f4076492f4ba7b972e430b
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2024-12-13 11:58:28 +0800

    修正结构

--- shortLink-admin/src/main/java/com/squirrel/shortLink/admin/service/impl/GroupServiceImpl.java
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/service/impl/GroupServiceImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.squirrel.shortLink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
@@ -7,11 +23,11 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.squirrel.shortLink.admin.common.convention.exception.ClientException;
+import com.squirrel.shortLink.admin.common.convention.exception.ServiceException;
+import com.squirrel.shortLink.admin.common.convention.result.Result;
 import com.squirrel.shortLink.admin.dao.entity.GroupUniqueDO;
 import com.squirrel.shortLink.admin.dao.mapper.GroupUniqueMapper;
-import com.squirrel.shortLink.common.convention.exception.ClientException;
-import com.squirrel.shortLink.common.convention.exception.ServiceException;
-import com.squirrel.shortLink.common.convention.result.Result;
 import com.squirrel.shortLink.admin.common.biz.user.UserContext;
 import com.squirrel.shortLink.admin.dao.entity.GroupDO;
 import com.squirrel.shortLink.admin.dao.mapper.GroupMapper;
@@ -31,7 +47,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
-import javax.sql.rowset.serial.SerialException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
--- shortLink-admin/src/main/java/com/squirrel/shortLink/admin/service/impl/RecycleBinServiceImpl.java
+++ shortLink-admin/src/main/java/com/squirrel/shortLink/admin/service/impl/RecycleBinServiceImpl.java
@@ -1,11 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.squirrel.shortLink.admin.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.squirrel.shortLink.common.convention.exception.ServiceException;
-import com.squirrel.shortLink.common.convention.result.Result;
 import com.squirrel.shortLink.admin.common.biz.user.UserContext;
+import com.squirrel.shortLink.admin.common.convention.exception.ServiceException;
+import com.squirrel.shortLink.admin.common.convention.result.Result;
 import com.squirrel.shortLink.admin.dao.entity.GroupDO;
 import com.squirrel.shortLink.admin.dao.mapper.GroupMapper;
 import com.squirrel.shortLink.admin.dto.req.ShortLinkRecycleBinPageReqDTO;
